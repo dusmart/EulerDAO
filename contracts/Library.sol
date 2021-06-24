9c266fd46354b3863ec429956b04b7b71e1ca909
vang1ong7ang <vang1ong7ang@outlook.com>
2021-06-24 09:26:44 +0000

    dev

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -6,8 +6,4 @@ library Library {
     function max(uint256 a, uint256 b) internal pure returns (uint256) {
         return a >= b ? a : b;
     }
-
-    function min(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
 }
